8673741a0e5af001fa20a698438a227a798a8d21
showkhin1997 <showkhin15@gmail.com>
2021-10-29 01:38:24 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6re1r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("tour_line_trip");
+        const servicesCollection = database.collection("services");
+        // create a document to insert
+        const services = {
+            title: "Record of a Shriveled Datum",
+            content: "No bytes, no problem. Just insert a document, in MongoDB",
+        }
+        const result = await servicesCollection.insertOne(services);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Torism Server Running');
