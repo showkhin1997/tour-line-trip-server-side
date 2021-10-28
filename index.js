df3a082a1adcb4bc6e1e2254bcfffbc1b569e208
showkhin1997 <showkhin15@gmail.com>
2021-10-29 01:43:14 +0600

    middle weare

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleweare
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6re1r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
