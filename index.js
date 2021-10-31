2a49a99106398f8059ef47b728331e1f6db25544
showkhin1997 <showkhin15@gmail.com>
2021-10-31 16:48:04 +0600

    user delete api

--- index.js
+++ index.js
@@ -101,6 +101,14 @@ async function run() {
             res.json(result);
         });
 
+        // DELETE API users from dashboard
+        app.delete('/confirmOrder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await confirmOrdersCollection.deleteOne(query);
+            res.json(result);
+        });
+
         // DELETE API
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
